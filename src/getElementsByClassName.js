aea297c4d0b430ccdfca2b11029d8849fa3891b7
Liza Lopez <liza.b.lopez@gmail.com>
2015-10-26 12:26:27 -0500

    Swapped for loops for _.each

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -9,22 +9,22 @@ var getElementsByClassName = function(className){
 //create inner function to insert a specific node as argument and automatically
 //run body as the first argument. The first loop checks the current Node for a match
 //with the target and pushes it onto an array, the second loop checks for children 
-//and recursively runs the inner function on each child node found. 
-  var nodeList = []; 
+//and recursively runs the inner function on each child node found.
+ 
+var nodeList = []; 
   function inner(currentNode) {
-
-    if (currentNode.classList) {
-        for (var i = 0; i < currentNode.classList.length; i++) {
-          if (currentNode.classList[i] == className){
+    if (currentNode.classList) { 
+      _.each(currentNode.classList, function(currentClass) {
+          if (currentClass === className){
             nodeList.push(currentNode);
           }
-        }
-      } 
-    if (currentNode.children.length > 0) {
-      for (var i=0; i<currentNode.children.length; i++) {
-        inner(currentNode.children[i]);
-      }
+        });
     } 
+    if (currentNode.children.length > 0) {
+      _.each(currentNode.children, function(currentChild) {
+        inner(currentChild);
+      });
+    }
     return nodeList;
   }
   return inner(document.body);
@@ -32,3 +32,4 @@ var getElementsByClassName = function(className){
 
 //You should use document.body, element.childNodes, and element.classList,
 //use children instead of childnodes, to exclude text and comment nodes
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -7,22 +7,20 @@
 var stringifyJSON = function(obj)  {
   //check if argument is complex or primitive
   //if primitive, just enclose with "" and print out -- base case
+  
   if (typeof(obj) !== "object" || (!obj)) {
       return (typeof(obj) == 'string') ?  "\""+obj+"\"" : ''+obj; 
   } else {
      //if it's an array, iterate accordingly through each type
     var arrayInput = [];
     if (Array.isArray(obj)) {
-      for (var i=0; i<obj.length; i++) {
-        //originally set (!obj[i]) but conditional statement did not account for negative integers
-        if (obj[i] === undefined) {
+      _.each(obj, function(element) {
+        if (element === undefined) {
           return '[]';
         } else {
-            //when looking inside itself, check if it is primitive or complex
-            //if it's complex, recurse on the current argumentd
-            arrayInput.push(stringifyJSON(obj[i]));  
-        } 
-      }
+          arrayInput.push(stringifyJSON(element));
+        }
+      })    
       //after the end of each array (or sub-array), enclose in brackets
         return "["+arrayInput+"]";
     } else {
