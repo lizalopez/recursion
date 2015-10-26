8e9327e21bb186a6b6195e50b7dc5664ecf4cf72
Liza Lopez <liza.b.lopez@gmail.com>
2015-10-26 11:58:58 -0500

    Complete - getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+
+var getElementsByClassName = function(className){
+//create inner function to insert a specific node as argument and automatically
+//run body as the first argument. The first loop checks the current Node for a match
+//with the target and pushes it onto an array, the second loop checks for children 
+//and recursively runs the inner function on each child node found. 
+  var nodeList = []; 
+  function inner(currentNode) {
+
+    if (currentNode.classList) {
+        for (var i = 0; i < currentNode.classList.length; i++) {
+          if (currentNode.classList[i] == className){
+            nodeList.push(currentNode);
+          }
+        }
+      } 
+    if (currentNode.children.length > 0) {
+      for (var i=0; i<currentNode.children.length; i++) {
+        inner(currentNode.children[i]);
+      }
+    } 
+    return nodeList;
+  }
+  return inner(document.body);
 };
+
+//You should use document.body, element.childNodes, and element.classList,
+//use children instead of childnodes, to exclude text and comment nodes
